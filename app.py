425bacdbdd00f712bd9035dabe226c7366105f57
MSAbhishek214 <abhishek.rotti755@gmail.com>
2021-07-03 20:28:58 +0530

    Module 7 practice with probability expectation

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
-print('Hello world!')
-print('Pushing to git')
+from scipy.stats import norm
+
+
+def get_cumulative_expectation(rewards, count):
+    pr = [val/sum(count) for val in count]
+    n = len(count)
+    cex = 0
+    for idx in range(n):
+        cex = cex + rewards[idx] * pr[idx]
+    return cex
+
+
+# probability computations
+rewards = norm.rvs(size=10)
+count = norm.rvs(size=10)
+
+cum_rewards = get_cumulative_expectation(rewards, count)
+print(cum_rewards)

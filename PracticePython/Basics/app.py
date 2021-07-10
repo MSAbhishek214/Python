95a79ffa5bc74be9e807c4e87b0f0b23b85583b5
MSAbhishek214 <abhishek.rotti755@gmail.com>
2021-07-10 11:00:33 +0530

    Add dynamic programming stub code

--- PracticePython/Basics/app.py
+++ PracticePython/Basics/app.py
@@ -16,3 +16,19 @@ count = norm.rvs(size=10)
 
 cum_rewards = get_cumulative_expectation(rewards, count)
 print(cum_rewards)
+
+#dynamic programming
+
+def fibonacci_dynamic_programming(n):
+    a = []
+    a.append(0)
+    a.append(1)
+    for i in range(2, n+1):
+        print(i, i-1, i-2)
+        a.append(a[i-1] + a[i-2])
+        print(a[i])
+    print(a)
+
+#call the function
+
+fibonacci_dynamic_programming(5)

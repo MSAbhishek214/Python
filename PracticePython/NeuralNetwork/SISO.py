0cdf3210d0a2d55551c0b241a5c6477f33050d6d
MSAbhishek214 <abhishek.rotti755@gmail.com>
2021-08-01 21:25:58 +0530

    Confusion with given and predicted value

--- PracticePython/NeuralNetwork/SISO.py
+++ PracticePython/NeuralNetwork/SISO.py
@@ -5,44 +5,56 @@ class SISO:
     #class variables
     weight = 0.0
     ALPHA = 0.1
-    output_y = 0.0
+    actual_y = 12
     
 
-    def __init__(self, weight, output_y):
+    def __init__(self, weight):
         self.weight = float(weight)
-        self.output_y = float(output_y)
 
 
-    def input_node(self, input_x):
-        return float(input_x)
+    def input_node(self, input_y):
+        return float(input_y)
 
 
-    def predict(self, input_x):
-        return input_x * self.weight
+    def predict(self, input_y):
+        return input_y * self.weight
 
 
-    def compare(self, output_y, predicted_y1):
-        return output_y - predicted_y1
+    def compare(self, given_y, predicted_y1):
+        return given_y - predicted_y1
 
 
     def learn(self, weight, delta):
         return weight + self.ALPHA * delta
 
 
+    def __repr__(self):
+        return f'''
+        A SISO object with following variables
+        weight: {self.weight}
+        Alpha value: {self.ALPHA}
+        actual value of y: {self.actual_y}
+        '''
+
+
 def train_NN(weight, y, epochs):
+    # initially adjusted weight will be weight itself, then after an epoch we get the new weight and feed the same to the network
     adjusted_weight = weight
+
+    # Loop through given number of epochs until we find no difference in the delta value (indicating learning complete)
     for i in range(epochs):
-        siso = SISO(adjusted_weight, y)
+        siso = SISO(adjusted_weight)
+        print(siso)
         input_y = siso.input_node(y)
         y1 = siso.predict(input_y)
         delta = siso.compare(y, y1)
         if delta == 0.00:
             break
         adjusted_weight = siso.learn(adjusted_weight, delta)
-        print(f'{i} --> y: {input_y}, y1: {y1}, delta: {delta:.2f}, weight: {weight}, adjusted weight: {adjusted_weight}')
+        print(f'{i+1} --> y: {input_y}, y1: {y1}, delta: {delta:.2f}, weight: {weight}, adjusted weight: {adjusted_weight}')
 
 
-weight = 0.1
-y = 12
-epochs = 10000000000000000000000
-train_NN(weight, y, epochs)
+weight = 21
+given_y = 21
+epochs = 1000000
+train_NN(weight, given_y, epochs)

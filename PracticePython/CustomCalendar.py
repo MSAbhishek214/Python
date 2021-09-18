565f89799c2970a395dc730acde699b7a4ad2a88
MSAbhishek214 <abhishek.rotti755@gmail.com>
2021-09-18 13:12:53 +0530

    PS Calendar

---
+++ PracticePython/CustomCalendar.py
@@ -0,0 +1,15 @@
+#Lets consider Sept 1 as starting day of the year
+
+day = 1
+week = 1
+days_in_week = ['Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue']
+months = ['Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
+month = 1
+quarter = 1
+DAYS_IN_WEEK = 7
+DAYS_IN_MONTH = 28
+
+user_input = input('Enter a date in DD/MM/YYYY format: ')
+
+user_date, user_month, user_year = user_input.split('/')
+
